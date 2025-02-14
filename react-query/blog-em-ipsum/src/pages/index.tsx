d539a102b79e8320ca8e14c8b0624bd2bc043801
LucasRohr <lucasrc17@live.com>
2025-02-14 16:45:01 -0300

    chore: :alembic: add react query devtools

--- react-query/blog-em-ipsum/src/pages/index.tsx
+++ react-query/blog-em-ipsum/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Posts } from "../components/posts";
 import "./App.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <div className="App">
         <h1>Blog &apos;em Ipsum</h1>
         <Posts />
