24634f18757a2e16f55acd6dc0aeda4f652dfd07
LucasRohr <lucasrc17@live.com>
2025-02-24 18:38:48 -0300

    feat: :sparkles: add blog pagination with query prefetch

--- react-query/blog-em-ipsum/src/components/posts/index.tsx
+++ react-query/blog-em-ipsum/src/components/posts/index.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useCallback, useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { PostDetail } from "../post-detail";
 import { PostInterface } from "../../interfaces/post";
@@ -8,10 +8,12 @@ import { PostsService } from "../../services/posts-service";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const [selectedPost, setSelectedPost] = useState<PostInterface>(null);
 
   const postsService = PostsService.getInstance();
+
+  const queryClient = useQueryClient();
 
   const {
     data: posts,
@@ -19,9 +21,23 @@ export function Posts() {
     isError,
     error,
   } = useQuery<PostInterface[]>({
-    queryKey: ["get-posts"],
-    queryFn: () => postsService.getPosts({ pageNum: 1 }),
+    queryKey: ["get-posts", currentPage],
+    queryFn: () => postsService.getPosts({ pageNum: currentPage }),
+    staleTime: 2000,
   });
+
+  useEffect(() => {
+    const hasNextPage = currentPage < maxPostPage;
+
+    if (hasNextPage) {
+      const nextPage = currentPage + 1;
+
+      queryClient.prefetchQuery({
+        queryKey: ["get-posts", nextPage],
+        queryFn: () => postsService.getPosts({ pageNum: nextPage }),
+      });
+    }
+  }, [currentPage, queryClient.prefetchQuery]);
 
   const renderPosts = useCallback(() => {
     const hasError = isError || !posts;
@@ -49,11 +65,21 @@ export function Posts() {
     <>
       <ul>{renderPosts()}</ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage == 1}
+          onClick={() => {
+            setCurrentPage((prevPage) => prevPage - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prevPage) => prevPage + 1);
+          }}
+        >
           Next page
         </button>
       </div>
